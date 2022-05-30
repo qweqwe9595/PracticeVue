4c7b23f449e93bedba37a372f2de339af4da9148
Pham Hoang Duy B1704719 <phduy1@tma.com.vn>
2022-05-30 11:01:48 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import cartpageVue from "./pages/cartPage.vue";
+import productPageVue from "./pages/productPage.vue";
+
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    routes: [
+        {path: '/', component: productPageVue},
+        {path: '/cart', component: cartpageVue}
+    ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/pages/productPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app>
+    asd
+  </v-app>
+</template>
